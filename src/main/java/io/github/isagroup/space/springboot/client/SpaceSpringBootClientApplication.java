879b0f1544681a2d6b20b11ee78e81d5bc6fc293
Pedro González Marcos <pedgonmar2@alum.us.es>
2025-06-30 12:47:20 +0200

    Spring template

---
+++ src/main/java/io/github/isagroup/space/springboot/client/SpaceSpringBootClientApplication.java
@@ -0,0 +1,13 @@
+package io.github.isagroup.space.springboot.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceSpringBootClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceSpringBootClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/isagroup/space/springboot/client/SpaceSpringBootClientApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.isagroup.space.springboot.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceSpringBootClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
